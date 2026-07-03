dd01bbbda23f5710db12a8d21489de84a17a49ba
God's Zeal † <godszealtecch@gmail.com>
2026-07-02 21:36:34 -0700

    Match send tab Dart formatter output

--- app/lib/pages/tabs/send_tab_vm.dart
+++ app/lib/pages/tabs/send_tab_vm.dart
@@ -127,9 +127,7 @@ final sendTabVmProvider = ViewProvider((ref) {
           builder: (_) => FavoriteDeleteDialog(favoriteDevice),
         );
         if (result == true) {
-          await ref.redux(favoritesProvider).dispatchAsync(
-                RemoveFavoriteAction(deviceFingerprint: device.fingerprint),
-              );
+          await ref.redux(favoritesProvider).dispatchAsync(RemoveFavoriteAction(deviceFingerprint: device.fingerprint));
         }
       } else {
         await showDialog(
