086ec61807b39bf7f9404ae55ac9fd079770fb5e
God's Zeal † <godszealtecch@gmail.com>
2026-07-03 04:47:51 -0700

    Add ZealBridge media bridge tab

--- app/lib/pages/home_page.dart
+++ app/lib/pages/home_page.dart
@@ -125,7 +125,7 @@ class _HomePageState extends State<HomePage> with Refena {
                                         SizedBox(height: 40)
                                       : SizedBox(height: 20),
                                   const Text(
-                                    'LocalSend',
+                                    'ZealBridge',
                                     style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
                                     textAlign: TextAlign.center,
                                   ),
